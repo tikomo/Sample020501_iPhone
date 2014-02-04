d0ccc1b756fa0b23463e65d9b30ef5cee44a4541
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-02-05 01:28:50 +0900

    Initial Commit

---
+++ Sample020501/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample020501
+//
+//  Created by tikomo on 2014/02/05.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
