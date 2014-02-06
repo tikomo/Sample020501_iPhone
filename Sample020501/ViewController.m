f3d1dbc6fcb343ae235f3cfedada539ac99687fa
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-02-06 14:36:18 +0900

    commit

--- Sample020501/ViewController.m
+++ Sample020501/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+// <- 黒丸クリックでどこと繋がっているか表示されます
+@property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *textFields;
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+- (IBAction)button_onClick:(UIButton *)sender;
+
 @end
 
 @implementation ViewController
@@ -18,6 +23,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // TextFieldのプレースフォルダにテキストを設定します
+    for (UITextField *fld in _textFields) {
+        
+        fld.text = @"AAA";
+        
+    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +40,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)button_onClick:(UIButton *)sender {
+    
+    // ソースだけだと、どことどう繋がっているか分からない...orz
+    
+    NSString *word;
+    // (UIButton *) を繋げる時に指定する事
+    switch (sender.tag) {
+    case 10:
+        word = @"AAAA";
+        break;
+    case 11:
+        word = @"BBBB";
+        break;
+    case 12:
+        word = @"CCCC";
+        break;
+    case 13:
+        word = @"DDDD";
+        break;
+    case 14:
+        word = @"EEEE";
+        break;
+    }
+    
+    _label1.text = word;
+    
+    _label1.text = sender.titleLabel.text;
+}
 @end
